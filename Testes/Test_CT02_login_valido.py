ee36ef5c5131aa9da5b1857347b281aeff1af3cc
Leonardo <diogoleo738@outlook.com>
2023-06-09 17:03:20 -0300

    usando page objects

--- Testes/Test_CT02_login_valido.py
+++ Testes/Test_CT02_login_valido.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
 import conftest
+from pages.login_page import LoginPage
 
 
 @pytest.mark.usefixtures("setup_teardown")
@@ -8,8 +9,7 @@ import conftest
 class TestCT02:
     def test_ct02_login_valido(self):
         driver = conftest.driver
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
+        login_page = LoginPage()
+        login_page.fazer_login("standard_user", "secret_sauce")
         assert driver.find_element(
             By.XPATH, "//span[@class='title']").is_displayed()
